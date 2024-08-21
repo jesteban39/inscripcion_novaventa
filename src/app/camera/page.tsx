7e9b2adc9ca9584f0e8000e57ef48dead1396a97
Esteban Quintero <juanesqb28@gmail.com>
2024-08-21 11:54:00 -0500

    camera 90%

--- src/app/camera/page.tsx
+++ src/app/camera/page.tsx
@@ -1,52 +1,46 @@
 'use client'
-
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import {useRouter} from 'next/navigation'
 import {useRef, useState} from 'react'
 import Webcam from 'react-webcam'
-
-const videoConstraints = {
-  width: 1280,
-  height: 720
-}
 
 export default function Camera() {
   const webcamRef = useRef<any>(null)
   const [imageSrc, setImageSrc] = useState(null)
   const [facingMode, setFacingMode] = useState<'user' | 'environment'>('environment')
+  const router = useRouter()
 
   return (
-    <div>
-      <div>
+    <Box>
+      <Box>
         <Webcam
           ref={webcamRef}
           audio={false}
+          width='90%'
           height={360}
           screenshotFormat='image/jpeg'
-          width={720}
-          videoConstraints={{
-            ...videoConstraints,
-            facingMode
-          }}
         />
-        <button>
-          <div
+        <Button>
+          <Box
             onClick={() => {
               const imageSrc = webcamRef.current.getScreenshot()
               setImageSrc(imageSrc)
             }}
           >
             Capture photo
-          </div>
-        </button>
-        <button
+          </Box>
+        </Button>
+        <Button
           onClick={() => {
             setFacingMode(facingMode === 'user' ? 'environment' : 'user')
           }}
         >
           Change Camera
-        </button>
-      </div>
-
+        </Button>
+        <Button onClick={() => router.push('/')}>{'Home'}</Button>
+      </Box>
       {imageSrc && <img src={imageSrc} alt='captured' />}
-    </div>
+    </Box>
   )
 }
